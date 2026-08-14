5224a72cc135d164618c1a04c92f4645fef50063
Vijay Kumar <kvijaykumar0822@gmail.com>
2026-08-14 11:32:07 +0530

    Update database connection for Aiven

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,16 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  connectTimeout: 30000,
 });
 
 db.connect((err) => {
